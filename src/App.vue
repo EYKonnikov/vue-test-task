1aa93bf385aa4e068789ed4c29ea373ab6aeb5a6
Test User <test@example.com>
2026-01-19 11:26:12 +0300

    Initial Vue 3 project setup

--- src/App.vue
+++ src/App.vue
@@ -1,120 +1,30 @@
 <script setup>
-import { ref } from 'vue'
-
-const userItems = [
-  { id: 1, name: "Shoes 1" },
-  { id: 2, name: "Shoes 2" },
-  { id: 3, name: "Shoes 3" },
-  { id: 4, name: "Shoes 4" },
-  { id: 5, name: "T-shirt 1" },
-  { id: 6, name: "T-shirt 2" },
-  { id: 7, name: "T-shirt 3" },
-  { id: 8, name: "T-shirt 4" }
-]
-
-const choiceItems = [
-  { id: 11, name: "Jacket 1" },
-  { id: 12, name: "Jacket 2" },
-  { id: 13, name: "Jacket 3" },
-  { id: 14, name: "Jacket 4" },
-  { id: 15, name: "Hoodie 1" },
-  { id: 16, name: "Hoodie 2" },
-  { id: 17, name: "Hoodie 3" },
-  { id: 18, name: "Hoodie 4" }
-]
-
-const selectedUser = ref([])
-const selectedChoice = ref(null)
-
-function selectUserItem(item) {
-  if (selectedUser.value.length < 6 && !selectedUser.value.some(i => i.id === item.id)) {
-    selectedUser.value.push(item)
-  }
-}
-
-function selectChoiceItem(item) {
-  selectedChoice.value = item
-}
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="container">
-    <div class="block top-left">
-      <h3>Selected User Items</h3>
-      <div v-for="item in selectedUser" :key="item.id" class="item">
-        {{ item.name }}
-      </div>
-    </div>
-    <div class="block top-right">
-      <h3>Selected Choice Item</h3>
-      <div v-if="selectedChoice" class="item">
-        {{ selectedChoice.name }}
-      </div>
-    </div>
-    <div class="block bottom-left">
-      <h3>User Items</h3>
-      <div v-for="item in userItems" :key="item.id" class="item selectable" @click="selectUserItem(item)">
-        {{ item.name }}
-      </div>
-    </div>
-    <div class="block bottom-right">
-      <h3>Choice Items</h3>
-      <div v-for="item in choiceItems" :key="item.id" class="item selectable" @click="selectChoiceItem(item)">
-        {{ item.name }}
-      </div>
-    </div>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.container {
-  display: grid;
-  grid-template-areas:
-    "top-left top-right"
-    "bottom-left bottom-right";
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto auto;
-  gap: 20px;
-  padding: 20px;
-  height: 100vh;
-  box-sizing: border-box;
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-.block {
-  border: 1px solid #ccc;
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-.top-left {
-  grid-area: top-left;
-}
-
-.top-right {
-  grid-area: top-right;
-}
-
-.bottom-left {
-  grid-area: bottom-left;
-}
-
-.bottom-right {
-  grid-area: bottom-right;
-}
-
-.item {
-  padding: 5px;
-  margin: 2px 0;
-  background-color: #f9f9f9;
-  border-radius: 4px;
-}
-
-.selectable {
-  cursor: pointer;
-}
-
-.selectable:hover {
-  background-color: #e0e0e0;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
